a368409c5057ffbcf2fca16a4c9f4fb7952dad00
Mael <maelsan@users.noreply.github.com>
2016-06-26 16:13:42 +0200

    https://github.com/maelsan/faker-push

---
+++ test.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+###
+ # NOTE: Put your informations here (path + folder name)
+ #     : If the folder is already created (or repo already cloned), nothing will be changed.
+###
+DIRGIT="$HOME/repos/"; # Path of your fake repo
+DIRNAME="faker-push/"; # Name of your repo
+BRANCH="master" # Branch name
+MESSAGE="https://github.com/maelsan/faker-push"
+
+FakerGo () {
+	if [ -d "$DIRGIT$DIRNAME" ]; then
+		if [ -d "$DIRGIT$DIRNAME".git ]; then
+			"q" > "$DIRGIT$DIRNAME".faker
+			git add *
+			git commit -m "$MESSAGE"
+			git push origin "$BRANCH"
+			FakerGo
+		fi
+	else
+		noFolder
+	fi
+}
+
+###
+ # Folder not available? We create him.
+###
+noFolder () {
+	cd "$DIRGIT" || exit
+	mkdir faker 
+	FakerGo
+}
+
+FakerGo # - Run script
